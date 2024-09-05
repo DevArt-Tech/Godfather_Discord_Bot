7c8d5de6f6e3e5dafe0d19f8f35c64e8a2e97c45
Arturo <arturobarbero97@gmail.com>
2024-09-05 14:40:40 +0200

    Sustituye members.name por display_name

--- online_members_command.py
+++ online_members_command.py
@@ -10,13 +10,13 @@ def create_json_habbo_response(guild):
     response_dict["title"] = "Miembros conectados a Habbo"
     response_dict["fields"] = list()
     for member in guild.members:
-        habbo_info = get_habbo_api_information(member.name)
+        habbo_info = get_habbo_api_information(member.display_name)
         if "online" in habbo_info and habbo_info["online"] is True:
             iso_date = habbo_info["lastAccessTime"]
             date_obj = datetime.strptime(iso_date, "%Y-%m-%dT%H:%M:%S.%f%z")
             formatted_date = date_obj.strftime("%Y-%m-%d")
             response_dict["fields"].append({
-                "Nombre": member.name,
+                "Nombre": member.display_name,
                 "Online": "Si" if habbo_info["online"] is True else "No",
                 "Ult. Conexión": formatted_date
             })
